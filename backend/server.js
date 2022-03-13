c17e333686f568c16c12363f94f114852298e704
Jacob Tupe <jadatu13@outlook.com>
2022-03-14 09:26:38 +1300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,26 @@
 const express = require("express");
-
+const articles = require("./dummydata/articles");
 const server = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
-server.listen(5000, console.log("Server is working and listening PORT 5000"));
+const PORT = process.env.PORT || 5001;
+
+server.get("/", (req, res) => {
+  res.send("API is running with nodemon.");
+});
+
+server.get("/api/articles", (req, res) => {
+  res.json(articles);
+});
+
+server.get("/api/articles/:id", (req, res) => {
+  const article = articles.find((n) => n._id === req.params.id);
+  res.send(article);
+  console.log(req.params);
+});
+
+server.listen(
+  PORT,
+  console.log(`Server is working and listening on PORT ${PORT}`)
+);
